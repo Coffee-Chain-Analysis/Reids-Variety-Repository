3253ef496b443ccae344ae08123ea36c9f5c36b4
Harsh Nagarkar <hknagark@go.olemiss.edu>
2019-12-02 17:14:57 -0600

    monthly and yearly change

--- final_project/stock_graphs.R
+++ final_project/stock_graphs.R
@@ -48,11 +48,34 @@ D_S %>%
   geom_smooth(mapping = aes(timestamp,close,color=stock),method = "lm")
 
 
-sim1_mod <- lm(close ~ timestamp, data = filter(D_S,stock=="DNKN"))
-coef(sim1_mod)
+SBUX<-SBUX %>% 
+  arrange(timestamp)
+DNKN<-DNKN %>% 
+  arrange(timestamp)
 
-sim1_mod <- lm(close ~ timestamp, data = filter(D_S,stock=="SBUX"))
-coef(sim1_mod)
+Starbucks_average_growth_rate<-(head(SBUX,1)$close/tail(SBUX,1)$close)/7*100
+
+Dunkin_average_growth_rate<-(head(DNKN,1)$close/tail(DNKN,1)$close)/7*100
+
+
+
+
+Starbucks_intercepts<-lm(SBUX$close~SBUX$timestamp)[1]$coefficient
+Starbucks_monthly_average_growth_rate<-(-atan(Starbucks_intercepts[1]/Starbucks_intercepts[2]))
+
+Dunkin_intercepts<-lm(DNKN$close~DNKN$timestamp)[1]$coefficient
+Dunkin_monthly_average_growth_rate<-(-atan(Dunkin_intercepts[1]/Dunkin_intercepts[2]))
+
+
+lm(DNKN$close~DNKN$timestamp) %>% 
+  Coefficients
+
+# 
+# sim1_mod <- lm(close ~ timestamp, data = filter(D_S,stock=="DNKN"))
+# coef(sim1_mod)
+# 
+# sim1_mod <- lm(close ~ timestamp, data = filter(D_S,stock=="SBUX"))
+# coef(sim1_mod)
 
 
 
